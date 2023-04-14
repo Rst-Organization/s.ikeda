147c11546d40d29f3aefa27faabc68becc2b58f4
t_shinnosuke <97178451+takayanagishinnosuke@users.noreply.github.com>
2023-04-14 13:40:32 +0000

    [modify] fsStreamを使用するに変更
    json文字数読み込み上限の為、エラーが発生。それらの修正を行いました。

--- firestore-import-export/data-conversion.js
+++ firestore-import-export/data-conversion.js
@@ -1,31 +1,33 @@
-const Fs  = require('fs');
+const fs = require('fs');
+const JSONStream = require('JSONStream');
 const { Parser } = require('json2csv');
-const data = require('./firestore-export.json');
 const jconv = require('jconv');
-const obj = []
 
-//id,tokenで整理したオブジェクトを配列に格納
-for(const key in data){
-  const item = data[key]
+const inputStream = fs.createReadStream('./firestore-export.json');
+const outputStream = fs.createWriteStream('export.csv');
 
-  for(const i in item){
-    //console.log(i,item[i]['token'])
+const jsonStream = JSONStream.parse('*');
+const fields = [
+  { label: 'ID', value: 'id' },
+  { label: '合計トークン', value: 'token' },
+];
+const json2csvParser = new Parser({ fields, header: true, withBOM: true });
+
+inputStream.pipe(jsonStream);
+
+const obj = [];
+jsonStream.on('data', (item) => {
+  for (const i in item) {
     obj.push({
-      id:i,
-      token:item[i]['token']
-    })
+      id: i,
+      token: item[i]['token'],
+    });
   }
-}
+});
 
-//CSV出力の際のヘッダーと値をセットしてCSVに変換
-const fields = [ 
-  { label: 'ID', value: 'id'},
-  { label: '合計トークン', value: 'token'},
-]
-const json2csvParser = new Parser({fields, header:true, withBOM: true});
-const parsedCsv = json2csvParser.parse(obj);
-
-//Shift-JISへ変換してCSVファイルとして出力
-jconv.convert(parsedCsv, 'UTF8', 'SJIS');
-Fs.writeFileSync("export.csv", parsedCsv);
-console.log('Done!')
+jsonStream.on('end', () => {
+  const parsedCsv = json2csvParser.parse(obj);
+  const convertedCsv = jconv.convert(parsedCsv, 'UTF8', 'SJIS');
+  outputStream.write(convertedCsv);
+  console.log('Done!');
+});

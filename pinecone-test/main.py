e0417f67ba3b2c0119145d952eabf0bc9bdb84ee
Shinnosuke Ikeda <s-ikeda@alterbooth.com>
2023-05-05 15:54:33 +0000

    2ndCommit

--- pinecone-test/main.py
+++ pinecone-test/main.py
@@ -38,8 +38,10 @@ len(df["Embedding"][0])
 #pineconeのインデックス作成
 pinecone.init(api_key=pineconeKey, environment="us-west1-gcp-free")
 
+#%%
 pinecone.create_index("example-index", dimension=embedding_dimension, metric="cosine")
 
+#%%
 pinecone_index = pinecone.Index("example-index")
 
 #%%
@@ -52,16 +54,18 @@ for i in df.index:
             (
                 str(df["id"][i]),
                 df["Embedding"][i],
-                {"ans": df["ans"][i], "um1": df["um1"][i]}
+                {"text": df["ans"][i], "source": df["Combined"][i]}
             )
         ],
     )
 
 #%%
-# クエリ検索用のベクトル
-embedding = get_embedding("子供の養育費", engine=MODEL)
+q = "子供が生まれたらまずは何をすればいいですか？"
+# クエリ検索用のベクトルを生成
+embedding = get_embedding(q, engine=MODEL)
 
 #%%
+# pineconeに作成したindexに対してクエリを発行 結果TOP3を表示
 res = pinecone_index.query(
         [embedding], 
         top_k=3, 
@@ -72,9 +76,108 @@ res = pinecone_index.query(
     )
 
 #%%
+#確認
+ansList = []
+
 for i in res["matches"]:
-    print(i["score"])
-    print(i["id"])
-    print(i["metadata"]["ans"])
-    print(i["metadata"]["um1"])
+    score = i["score"]
+    id = i["id"]
+    text = i["metadata"]["text"]
+    source = i["metadata"]["Combined"]
+
+    ansList.append(text)
+    print(f"id: {id}")
+    print(f"score: {score}")
+    print(f"text: {text}")
+    print(f"source: {source}")
     print("------")
+
+#%%
+print(ansList)
+
+#%%
+# 検索結果をgptに返答させる。※ davinciの場合
+response = openai.Completion.create(
+    engine="text-davinci-003",
+    prompt=f"Please answer the question in Japanese, taking into account the following context. \
+        Context: \
+            Question: {q} \
+            Answer: {ansList[0]},{ansList[1]}",
+    temperature=0,
+    max_tokens=400
+)
+print(response.choices[0]["text"])
+
+#%%
+#検索結果をgptに返答させる。gpt-3.5-turboの場合
+
+prompt_prefix = \
+f"市You are an AI assistant supporting questions about City Hall. Please answer questions in Japanese. \
+If you do not have sufficient information, answer don't know."
+
+response = openai.ChatCompletion.create(
+    model="gpt-3.5-turbo",
+    messages=[
+        {"role": "system", "content": prompt_prefix },
+        {"role": "assistant", "content": f"{ansList[0]},{ansList[1]}" },
+        {"role": "user", "content": "Please extract only the important parts of this answer and explain in Japanese."},
+    ],
+)
+print(response.choices[0]["message"]["content"].strip())
+
+#%%
+#LangChainでクエリ ＆ GPTで返答
+# 必要なモジュールを追加
+from langchain import VectorDBQA, OpenAI, VectorDBQAWithSourcesChain
+from langchain.embeddings.openai import OpenAIEmbeddings
+from langchain.vectorstores import Pinecone
+
+#%%
+# LangChain用にembeddingsを再度初期化
+OPENAI_API_KEY = os.environ.get("OPENAIKEY")
+embeddings = OpenAIEmbeddings(openai_api_key=OPENAI_API_KEY)
+#%%
+#pineconeの初期設定を再度
+pinecone.init(api_key=pineconeKey, environment="us-west1-gcp-free")
+
+#%%
+#すでにあるindexを指定してベクトルストアを指定してベクトル検索の基盤を設定
+docsearch = Pinecone.from_existing_index("example-index", embeddings)
+
+#%%
+# まずは通常にクエリ 類似度TOP3
+query = "子供が生まれたら何をすればいいですか？"
+docs = docsearch.similarity_search(query,k=3)
+# %%
+# 上記(いつもどおりの結果)
+print(docs)
+
+#%%
+# 検索結果をgptに返答させる。langchainを使ったパターン
+# OpenAIのモデルのセットアップ
+llm = OpenAI(
+    openai_api_key=OPENAI_API_KEY,
+    model_name='gpt-3.5-turbo',
+    temperature=0.0
+)
+
+#%%
+# VectorDBQAChainのセットアップ
+chain = VectorDBQA.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    vectorstore=docsearch,
+)
+#%%
+# クエリ＆返答
+chain.run("子供が生まれたら何をすればいいですか？")
+
+#%%
+# 返答のソースを確認
+sources = VectorDBQAWithSourcesChain.from_chain_type(
+    llm=llm,
+    chain_type="stuff",
+    vectorstore=docsearch
+)
+
+sources("子供が生まれたら何をすればいいですか？")

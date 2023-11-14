06f93e163b235955f91755ba05ca0cae245d4f07
Shinnosuke Ikeda <s-ikeda@alterbooth.com>
2023-11-14 10:45:52 +0900

    seacret delete

--- aws-cdk/cdk-stepfunction/cdk/bin/cdk-stepfunction.ts
+++ aws-cdk/cdk-stepfunction/cdk/bin/cdk-stepfunction.ts
@@ -5,10 +5,9 @@ import { StepfunctionsRestApiStack } from '../lib/cdk-stepfunction-stack';
 import { LambdaRestApiStack } from '../lib/lambdaChain';
 
 const app = new cdk.App();
-const channelAccessToken =
-  '3ktUXkqvdWyYsMUq1SdDtFISj2XkIvUeeSpEDrCObRjZ8H6y4/anBtxDkzbxXkLbodwhiSaSk4QnIe1IoPiE4izCFzxV2zpC/6ODwi1vhLVuT6ugEOfTFzSRV/0bmV/At14THxmsvz/6rbd408UDYAdB04t89/1O/w1cDnyilFU=';
-const channelSecret = '5e2cc09684c3841cfd7ef98922dee7a4';
-const openaiApiKey = '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';
+const channelAccessToken = '';
+const channelSecret = '';
+const openaiApiKey = '';
 
 new StepfunctionsRestApiStack(app, 'CdkStepfunctionStack', {
   env: {
